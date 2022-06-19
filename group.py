13a831ed10719103ae3e42625e1f01d390c62d38
gbyorn <byorn.gosu@gmail.com>
2022-06-19 19:10:01 +0300

    Добавлен вспомогательный класс для хранения свойств групп.

---
+++ group.py
@@ -0,0 +1,5 @@
+class Group:
+    def __init__(self, group_name: str, group_header: str, group_footer: str):
+        self.group_name = group_name
+        self.group_header = group_header
+        self.group_footer = group_footer
