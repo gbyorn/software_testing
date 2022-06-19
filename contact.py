f02fe754e76d4a1145f209002b32aeedce820c78
gbyorn <byorn.gosu@gmail.com>
2022-06-19 20:49:34 +0300

    Создан вспомогательный класс для хранения свойств контакта.

---
+++ contact.py
@@ -0,0 +1,31 @@
+class Contact:
+    def __init__(self, first_name: str, middle_name: str, last_name: str, nickname: str, title: str,
+                 company: str, address: str, home_phone: str, mobile_phone: str, work_phone: str, fax: str,
+                 first_email: str, second_email: str, third_email: str, homepage: str, b_day: str, b_month: str,
+                 b_year: str, a_day: str, a_month: str, a_year: str, second_address: str, second_home: str,
+                 second_notes: str, photo: str):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.first_email = first_email
+        self.second_email = second_email
+        self.third_email = third_email
+        self.homepage = homepage
+        self.b_day = b_day
+        self.b_month = b_month
+        self.b_year = b_year
+        self.a_day = a_day
+        self. a_month = a_month
+        self.a_year = a_year
+        self.second_address = second_address
+        self.second_home = second_home
+        self.second_notes = second_notes
+        self.photo = photo
