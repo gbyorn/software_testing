93bb10c9da92318160ecf38d385291b2d32502a5
gbyorn <byorn.gosu@gmail.com>
2022-07-17 15:34:21 +0300

    Добавлена рандомизация данных в тесты создания групп и контактов.

--- Test/test_create_contact.py
+++ Test/test_create_contact.py
@@ -1,17 +1,34 @@
 from Model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_create_contact(app):
+def random_string(prefix, max_len):
+    symbols = string.ascii_letters + string.digits + string.punctuation + (" " * 10)
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_len))])
+
+
+test_data = [Contact(first_name=random_string("firstname", 10), middle_name=random_string("middlename", 10),
+                     last_name=random_string("lastname", 10), nickname=random_string("nickname", 10),
+                     title=random_string("title", 15), company=random_string("title", 15),
+                     address=random_string("title", 20), home_phone=random_string("h", 5),
+                     mobile_phone=random_string("m", 5), work_phone=random_string("w", 5),
+                     fax=random_string("fax", 5), first_email=random_string("first", 5),
+                     second_email=random_string("second", 5), third_email=random_string("third", 5),
+                     homepage=random_string("homepage", 10), b_day=str(random.randint(1, 31)),
+                     b_month=random.choice(['October', 'April', 'May']), b_year=str(random.randint(1980, 2010)),
+                     a_day=str(random.randint(1, 31)), a_month=random.choice(['November', 'June', 'July']),
+                     a_year=str(random.randint(1980, 2010)), second_address=random_string("secondaddr", 10),
+                     second_home=random_string("s", 5), second_notes=random_string("secondnote", 10))
+             for i in range(1)]
+
+
+@pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
+def test_create_contact(app, contact):
     app.contact.open_addresses_home_page()
     old_contacts = app.contact.get_contacts_list()
-    contact = Contact(first_name='TestFirstname', middle_name='TestMiddleName',
-                      last_name='TestLastName', nickname='TestNickName', title='TestTitle',
-                      company='TestCompany', address='TestAddress', home_phone='6666666',
-                      mobile_phone='19999999999', work_phone='7777777', fax='123',
-                      first_email='first@mail.ru', second_email='second@mail.ru',
-                      third_email='third@mail.ru', homepage='TestHomepage', b_day='10',
-                      b_month='October', b_year='1990', a_day='10', a_month='October', a_year='2010',
-                      second_address='TestSecAddress', second_home='TestSecHome', second_notes='TestSecNotes')
+    contact = contact
     app.contact.create_contact(contact)
     app.contact.open_addresses_home_page()
     new_contacts = app.contact.get_contacts_list()
--- Test/test_create_group.py
+++ Test/test_create_group.py
@@ -1,10 +1,24 @@
 from Model.group import Group
+import pytest
+import random
+import string
 
 
-def test_create_group(app):
+def random_string(prefix, max_len):
+    symbols = string.ascii_letters + string.digits + string.punctuation + (" " * 10)
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_len))])
+
+
+test_data = [Group(group_name=random_string("name", 10), group_header=random_string("header", 20),
+                   group_footer=random_string("footer", 20))
+             for i in range(1)]
+
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_create_group(app, group):
+    print(group)
     app.group.open_groups_page()
     old_groups = app.group.get_group_list()
-    group = Group(group_name='TestGroup', group_header='TestHeader', group_footer='TestFooter')
     app.group.create_group(group)
     app.group.open_groups_page()
     new_groups = app.group.get_group_list()
