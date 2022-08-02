e84e6760f64f08db1111666dd8e742022fbbfac9
gbyorn <byorn.gosu@gmail.com>
2022-08-02 22:00:20 +0300

    Добавлен тест на сравнение контактов с информацией из БД.

---
+++ Fixture/orm.py
@@ -0,0 +1,61 @@
+from pony.orm import *
+from datetime import datetime
+from Model.group import Group
+from Model.contact import Contact
+from pymysql.converters import decoders
+
+
+class ORMFixture:
+
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = 'group_list'
+        group_id = PrimaryKey(int, column='group_id')
+        group_name = Optional(str, column='group_name')
+        group_header = Optional(str, column='group_header')
+        group_footer = Optional(str, column='group_footer')
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        contact_id = PrimaryKey(int, column='id')
+        first_name = Optional(str, column='firstname')
+        last_name = Optional(str, column='lastname')
+        address = Optional(str, column='address')
+        home_phone = Optional(str, column='home')
+        mobile_phone = Optional(str, column='mobile')
+        work_phone = Optional(str, column='work')
+        second_home = Optional(str, column='phone2')
+        first_email = Optional(str, column='email')
+        second_email = Optional(str, column='email2')
+        third_email = Optional(str, column='email3')
+        deprecated = Optional(str, column='deprecated')
+
+    def __init__(self, host, name, user, password):
+        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+        self.db.generate_mapping()
+
+    def convert_groups_to_model(self, groups):
+        def convert(group: Group):
+            return Group(group_id=str(group.group_id), group_name=group.group_name,
+                         group_header=group.group_header, group_footer=group.group_footer)
+        return list(map(convert, groups))
+
+    @db_session
+    def get_group_list(self):
+        return self.convert_groups_to_model(select(group for group in ORMFixture.ORMGroup))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact: Contact):
+            return Contact(contact_id=str(contact.contact_id), first_name=contact.first_name.strip(),
+                           last_name=contact.last_name.strip(), address=contact.address.strip(),
+                           home_phone=contact.home_phone, work_phone=contact.work_phone,
+                           mobile_phone=contact.mobile_phone, second_home=contact.second_home,
+                           first_email=contact.first_email, second_email=contact.second_email,
+                           third_email=contact.third_email)
+        return list(map(convert, contacts))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(select(contact for contact in ORMFixture.ORMContact
+                                                     if contact.deprecated is None))
--- Test/test_compare_contacts.py
+++ Test/test_compare_contacts.py
@@ -1,5 +1,9 @@
 import re
 from Model.contact import Contact
+from Fixture.orm import ORMFixture
+
+
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 
 def test_compare_contacts(app):
@@ -10,6 +14,19 @@ def test_compare_contacts(app):
     assert contact_from_home_page.address == contact_from_edit_page.address.strip()
     assert contact_from_home_page.all_emails_from_home_page == merge_emails(contact_from_edit_page)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones(contact_from_edit_page)
+
+
+def test_compare_contacts_db(app):
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_home_page = sorted(app.contact.get_contacts_list(), key=Contact.id_or_max)
+    for contact_index in range(len(contacts_from_db)):
+        assert contacts_from_db[contact_index].first_name == contacts_from_home_page[contact_index].first_name
+        assert contacts_from_db[contact_index].last_name == contacts_from_home_page[contact_index].last_name
+        assert contacts_from_db[contact_index].address == contacts_from_home_page[contact_index].address
+        assert merge_phones(contacts_from_db[contact_index]) \
+               == contacts_from_home_page[contact_index].all_phones_from_home_page
+        assert merge_emails(contacts_from_db[contact_index]) \
+               == contacts_from_home_page[contact_index].all_emails_from_home_page
 
 
 def clear(data: str) -> str:
