49a8fac7e106347df6c692c4262dcf5a0db0e5b7
gbyorn <byorn.gosu@gmail.com>
2022-08-20 15:38:12 +0300

    Внесены правки для поддержки python ниже 4.10

--- Model/contact.py
+++ Model/contact.py
@@ -2,16 +2,12 @@ from sys import maxsize
 
 
 class Contact:
-    def __init__(self, first_name: str | None = None, middle_name: str | None = None, last_name: str | None = None,
-                 nickname: str | None = None, title: str | None = None, company: str | None = None,
-                 address: str | None = None, home_phone: str | None = None, mobile_phone: str | None = None,
-                 work_phone: str | None = None, fax: str | None = None, first_email: str | None = None,
-                 second_email: str | None = None, third_email: str | None = None, homepage: str | None = None,
-                 b_day: str | None = None, b_month: str | None = None, b_year: str | None = None,
-                 a_day: str | None = None, a_month: str | None = None, a_year: str | None = None,
-                 second_address: str | None = None, second_home: str | None = None, second_notes: str | None = None,
-                 contact_id: str | None = None, all_phones_from_home_page=None, all_emails_from_home_page=None,
-                 group_name: str | None = None):
+    def __init__(self, first_name=None, middle_name=None, last_name=None, nickname=None, title=None,
+                 company=None, address=None, home_phone=None, mobile_phone=None, work_phone=None, fax=None,
+                 first_email=None, second_email=None, third_email=None, homepage=None, b_day=None, b_month=None,
+                 b_year=None, a_day=None, a_month=None, a_year=None, second_address=None, second_home=None,
+                 second_notes=None, contact_id=None, all_phones_from_home_page=None, all_emails_from_home_page=None,
+                 group_name=None):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
--- Model/group.py
+++ Model/group.py
@@ -2,8 +2,7 @@ from sys import maxsize
 
 
 class Group:
-    def __init__(self, group_name: str | None  = None, group_header: str | None = None, group_footer: str | None = None,
-                 group_id=None):
+    def __init__(self, group_name=None, group_header=None, group_footer=None, group_id=None):
         self.group_name = group_name
         self.group_header = group_header
         self.group_footer = group_footer
